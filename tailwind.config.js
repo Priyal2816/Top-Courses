3fc75a47de1bb0bd72f8034d16224c3bee84ec58
Priyal Khandelwal <priyalkhandelwal28@gmail.com>
2025-08-15 19:12:00 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: "#1e293b",
+        bgDark2: "#334155",  
+      },
+    },
+  },
+  plugins: [],
+}
+
